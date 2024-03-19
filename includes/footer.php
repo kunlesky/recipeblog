bb49027cbe981f1b94e593464daae9ee5bf7adca
kunlesky <adepegbaadegoke5757@gmail.com>
2024-03-19 14:01:22 +0000

    Created the homepage with the header and footer separated

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+                        <p>Goke Food Recipe &copy; <?php echo date('Y'); ?></p>
+                </div>
---
+++ index.php
@@ -0,0 +1,34 @@
+
+        <?php require_once('includes/header.php') ?>
+        <title>icanbe's Recipe Blog | Home </title>
+</head>
+<body>
+        <!-- container - wraps whole page -->
+        <div class="container">
+                <?php include('C:\xampp\htdocs\recipeblog\includes\nav.php') ?>
+                <?php include( 'C:\xampp\htdocs\recipeblog\includes\banner.php') ?>
+
+                <!-- Page content -->
+                <div class="content">
+                        <h2 class="content-title">Recent Recipe</h2>
+                        <hr> 
+                        <hr>
+                <!-- more content still to come here ... -->
+
+                <!-- Add this ... -->
+                
+                                <!-- // Page content -->
+                                
+                
+
+                <!-- footer -->
+                <?php include('includes/footer.php') ?>
+               
+                <!-- // footer -->
+
+        </div>
+        <!-- // container -->
+</body>
+</html>
+
+
